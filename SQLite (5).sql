3bfd3d106d5768debf67e16f74826e1c960b2ad9
Victor Vaz <134503953+vaxzzzz@users.noreply.github.com>
2023-10-31 08:39:00 -0300

    Add files via upload

---
+++ SQLite (5).sql
@@ -0,0 +1,99 @@
+
+CREATE TABLE COISADO (
+
+ID_NF INT NOT NULL,
+
+ID_ITEM INT NOT NULL,
+
+COD_PROD INT NOT NULL,
+
+VALOR_UNIT FLOAT (4) NOT NULL,
+
+QUANTIDADE varchar(2) NOT NULL,
+
+DESCONTO char(2));
+
+
+   INSERT INTO COISA (id_nf, id_item, cod_prod, valor_unit, quantidade, desconto)           
+
+VALUES ("1", "1", "1", "25,00", "10", "5")
+
+
+     INSERT INTO COISA (id_nf, id_item, cod_prod, valor_unit, quantidade, desconto)
+
+VALUES ("1", "2", "2", "13,50", "3", "NULL")
+
+ 
+
+   INSERT INTO COISA (id_nf, id_item, cod_prod, valor_unit, quantidade, desconto)                   
+
+VALUES ("2", "1", "3", "15,00", "4", "NULL")
+
+
+     INSERT INTO COISA (id_nf, id_item, cod_prod, valor_unit, quantidade, desconto)
+
+VALUES ("2", "2", "4", "10,00", "5", "NULL")
+
+ 
+
+   INSERT INTO COISA (id_nf, id_item, cod_prod, valor_unit, quantidade, desconto)           
+
+VALUES ("3", "1", "1", "25,00", "5", "NULL")
+
+
+     INSERT INTO COISA (id_nf, id_item, cod_prod, valor_unit, quantidade, desconto)
+
+VALUES ("3", "2", "4", "10,00", "4", "NULL")
+
+ 
+
+   INSERT INTO COISA (id_nf, id_item, cod_prod, valor_unit, quantidade, desconto) 
+
+VALUES ("4", "1", "5", "30,00", "10", "15")
+
+
+     INSERT INTO COISA (id_nf, id_item, cod_prod, valor_unit, quantidade, desconto)
+
+VALUES ("4", "2", "4", "10,00", "12", "5")
+
+ 
+
+   INSERT INTO COISA (id_nf, id_item, cod_prod, valor_unit, quantidade, desconto) 
+
+VALUES ("5", "1", "3", "15,00", "3", "NULL")
+
+
+     INSERT INTO COISA (id_nf, id_item, cod_prod, valor_unit, quantidade, desconto)
+
+VALUES ("5", "2", "5", "30,00", "6", "NULL")
+
+ 
+
+   INSERT INTO COISA (id_nf, id_item, cod_prod, valor_unit, quantidade, desconto) 
+
+VALUES ("6", "1", "1", "25,00", "22", "15")
+
+
+     INSERT INTO COISA (id_nf, id_item, cod_prod, valor_unit, quantidade, desconto)
+
+VALUES ("6", "2", "3", "15,00", "25", "20")
+
+ 
+
+   INSERT INTO COISA (id_nf, id_item, cod_prod, valor_unit, quantidade, desconto) 
+
+VALUES ("7", "1", "1", "25,00", "10", "3")
+
+
+     INSERT INTO COISA (id_nf, id_item, cod_prod, valor_unit, quantidade, desconto)
+
+VALUES ("7", "2", "2", "13,50", "10", "4")
+
+ 
+
+SELECT * FROM COISA
+
+
+   
+   
+   
